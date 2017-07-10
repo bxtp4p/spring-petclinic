2b8c628b92e3eba43be411189a44838c2bd5bb52
Bart X Tubalinal <bart.tubalinal@deviantpoint.com>
2017-07-10 10:25:50 -0500

    Updated jenkins build file with latest changes.

--- jenkins-post-build-step.sh
+++ jenkins-post-build-step.sh
@@ -1,17 +1,18 @@
-export DOCKER_IMAGE_NAME=bxtp4p/spring-petclinic
+export DOCKER_IMAGE_NAME=bxt-ddr.centralus.cloudapp.azure.com:5001/dockerdemoorg/spring-petclinic
 export DOCKER_IMAGE_NAME_POM=$DOCKER_IMAGE_NAME:$POM_VERSION
 export DOCKER_DB_IMAGE_NAME=$DOCKER_IMAGE_NAME-db
 export DOCKER_DB_IMAGE_NAME_POM=$DOCKER_DB_IMAGE_NAME:$POM_VERSION
-
-#Log in to Docker
-sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+export COMPOSE_FILE=./docker-compose.ucp.yml
 
 #Build using Docker Compose
-sudo docker-compose build
+sudo docker-compose -f $COMPOSE_FILE build
 
 #Create tags for the POM version
 sudo docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME_POM
 sudo docker tag $DOCKER_DB_IMAGE_NAME $DOCKER_DB_IMAGE_NAME_POM
+
+#Log in to DTR
+sudo docker login https://bxt-ddr.centralus.cloudapp.azure.com:5001 -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 #Push the images to the registry
 sudo docker push $DOCKER_IMAGE_NAME
@@ -19,10 +20,11 @@ sudo docker push $DOCKER_IMAGE_NAME_POM
 sudo docker push $DOCKER_DB_IMAGE_NAME
 sudo docker push $DOCKER_DB_IMAGE_NAME_POM
 
-#Use Docker Compose to deploy services to the cluster
-export DOCKER_HOST=tcp://127.0.0.1:32768
-docker stack deploy -c ./docker-compose.yml spring-petclinic
-DOCKER_HOST=
+#Log out of DTR
+sudo docker logout https://bxt-ddr.centralus.cloudapp.azure.com:5001
 
-#Log out of Docker
-sudo docker logout
+#Deploy to UCP
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH="/ucp/ucp-cli-client"
+export DOCKER_HOST=tcp://bxt-ucp.centralus.cloudapp.azure.com:443
+docker stack deploy -c $COMPOSE_FILE spring-petclinic
